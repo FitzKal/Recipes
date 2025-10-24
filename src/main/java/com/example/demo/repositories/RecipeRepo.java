90d82a9ca6313144b18f498b4979553c2b35876c
Kulcsár Gerzson <zsoncy@mailbox.unideb.hu>
2025-10-24 21:10:55 +0300

    Implemented the RecipeRepo for the recipe service

--- src/main/java/com/example/demo/repositories/RecipeRepo.java
+++ src/main/java/com/example/demo/repositories/RecipeRepo.java
@@ -1,4 +1,9 @@
 package com.example.demo.repositories;
 
-public interface RecipeRepo {
+import com.example.demo.model.Recipe;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecipeRepo extends JpaRepository<Recipe, Long> {
 }
