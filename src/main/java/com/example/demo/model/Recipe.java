482a786f366d116eabaad15b9baabe38972fa45b
Kulcsár Gerzson <zsoncy@mailbox.unideb.hu>
2025-10-24 20:55:00 +0300

    Implemented the recipe model alongside the category enum

--- src/main/java/com/example/demo/model/Recipe.java
+++ src/main/java/com/example/demo/model/Recipe.java
@@ -2,17 +2,33 @@ package com.example.demo.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Builder
+@Entity
+@Table(name = "recipes")
 public class Recipe {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    Long id;
+    private Long id;
+
+    @Column(nullable = false)
+    private String recipeTitle;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private Category category;
+
+    @Column(nullable = false)
+    private String pictureSrc;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "user_id", nullable = false)
