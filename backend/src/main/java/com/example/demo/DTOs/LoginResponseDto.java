96ff27a33ae363d7c4ba72139487897bf2d6c6e2
Kovács Veronika <sajtoskenyer123@mailbox.unideb.hu>
2025-10-29 14:02:23 +0100

    Added DTOs for Auth (Register/Login/Response)

---
+++ backend/src/main/java/com/example/demo/DTOs/LoginResponseDto.java
@@ -0,0 +1,14 @@
+package com.example.demo.DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LoginResponseDto {
+    private String username;
+}
