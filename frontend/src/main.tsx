18d33215bd8f9758c96042f2a3afd931febb59b6
Vesa Bence Janos <vesabence@mailbox.unideb.hu>
2025-10-29 13:25:07 +0100

    Added Tailwind css to the project dependencies

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/main.css';
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import {createBrowserRouter, RouterProvider} from 'react-router-dom';
@@ -6,6 +8,7 @@ import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import LoginForm from "./components/LoginForm.tsx";
 import RegisterForm from "./components/RegisterForm.tsx";
 import App from "./App.tsx";
+
 
 const router = createBrowserRouter([{
     path:'/',
