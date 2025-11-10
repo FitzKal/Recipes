7535c0d482fb51cf342ce4a4fa75e1b612335662
Vesa Bence Janos <vesabence@mailbox.unideb.hu>
2025-11-10 12:55:26 +0200

    Implemented delete functionality in the user component

--- frontend/src/components/Admin/User.tsx
+++ frontend/src/components/Admin/User.tsx
@@ -2,12 +2,14 @@ import type {userProp} from "../../Types/User.ts";
 import {userStore} from "../../Stores/UserStore.ts";
 import {useMutation, useQueryClient} from "@tanstack/react-query";
 import {toast} from "react-toastify";
+import {userDelete} from "../../service/AdminService.ts";
+import {useEffect} from "react";
 
 export default function User(prop:userProp){
     const currentUser = userStore.getState().user;
     const queryClient = useQueryClient();
 
-    /*const deleteMutation = useMutation({
+    const deleteMutation = useMutation({
         mutationFn: (id:number) => {
             const token = userStore.getState().user?.accessToken;
             if (!token) {
@@ -33,7 +35,12 @@ export default function User(prop:userProp){
     }
 
     const isOwnUser = currentUser?.username === prop.userInfo.username;
-    const canDelete = !isOwnUser && prop.userInfo.userId != null;*/
+    const canDelete = !isOwnUser && prop.userInfo.user_id != null;
+
+    useEffect(() => {
+        console.log(canDelete);
+        console.log(isOwnUser);
+    }, []);
 
     return(
         <tr className="border-b-2 bg-blue-100">
@@ -43,6 +50,9 @@ export default function User(prop:userProp){
             <td><button className={"rounded-2xl pr-2 pl-2 bg-blue-800 " +
                 "text-white transition delay-50 ease-in-out hover:bg-blue-500"}
                         >Update</button></td>
+           <td>{canDelete ? <button className={"pr-2 pl-2 rounded-2xl w-20 bg-red-800 " +
+                "text-white transition delay-50 ease-in-out hover:bg-red-500"} onClick={() => handleDelete(prop.userInfo.user_id!)}>Delete</button>
+            :<></>}</td>
         </tr>
     );
 }
