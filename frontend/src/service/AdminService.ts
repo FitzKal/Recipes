f450b18f16febd3248ce2747ab0c5c1d4482f63d
Vesa Bence Janos <vesabence@mailbox.unideb.hu>
2025-11-10 13:06:02 +0200

    Implemented update method in the AdminService.ts

--- frontend/src/service/AdminService.ts
+++ frontend/src/service/AdminService.ts
@@ -1,5 +1,7 @@
 // ------------- GetAll -------------
 
+import type {UserResponse} from "../Types/User.ts";
+
 export const usersGetAll = async (accessToken:string) => {
     const res = await fetch("/api/recipe/user",{
         headers:{
@@ -29,5 +31,26 @@ export const userDelete = async (accessToken:string,id:number) =>{
     }else {
         const message = await res.text();
         throw new Error(message || "Request could not be completed");
+    }
+}
+
+// ------------- Update -------------
+
+export const userUpdate = async (accessToken:string, id:number, data:UserResponse)=> {
+    const res = await fetch(`/api/recipe/user/${id}`, {
+        method: "PUT",
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data)
+    });
+    if (res.ok) {
+        const response = await res.json();
+        console.log(response);
+        return response;
+    } else {
+        const message = await res.text();
+        throw new Error(message || "Could not complete request");
     }
 }
