d50b8f06bdc061379c00219efbeff8b3a65f893f
Vesa Bence Janos <vesabence@mailbox.unideb.hu>
2025-11-10 12:55:53 +0200

    Adjusted the json properties of the BasicUserDto class

--- backend/src/main/java/com/example/demo/DTOs/BasicUserDto.java
+++ backend/src/main/java/com/example/demo/DTOs/BasicUserDto.java
@@ -2,6 +2,8 @@ package com.example.demo.DTOs;
 
 import com.example.demo.model.Recipe;
 import com.example.demo.model.Role;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,9 +17,11 @@ import java.util.List;
 @Builder
 @AllArgsConstructor
 public class BasicUserDto {
+    @JsonProperty("user_id")
     private Long userId;
     private String username;
     private String password;
+    @JsonIgnore
     private List<Recipe> recipes;
     private Role role;
 }
