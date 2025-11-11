280adfd130d593f06d7fc3d473569419e5b21ec7
Vesa Bence Janos <vesabence@mailbox.unideb.hu>
2025-11-11 14:36:02 +0100

    Made the title clickable to access recipe details

--- frontend/src/components/Recipe/Recipe.tsx
+++ frontend/src/components/Recipe/Recipe.tsx
@@ -1,4 +1,5 @@
 import type {recipeType} from "../../Types/Recipe.ts";
+import {Link} from "react-router-dom";
 
 
 
@@ -6,7 +7,7 @@ export default function Recipe(recipeProp: { recipeInfo: recipeType }) {
 
     return (
         <div className={"flex flex-col w-60"}>
-            <p className={"text-2xl mb-3 transition delay-50 ease-in-out hover:text-blue-700"}>{recipeProp.recipeInfo.recipeTitle}</p>
+            <Link to={`/dashboard/recipes/${recipeProp.recipeInfo.id}`} className={"text-2xl mb-3 transition delay-50 ease-in-out hover:text-blue-700"}>{recipeProp.recipeInfo.recipeTitle}</Link>
             <img className={"h-80 w-55 mb-2 transition delay-150 duration-300 ease-in-out hover:scale-130"}
                  src={recipeProp.recipeInfo.pictureSrc} alt={recipeProp.recipeInfo.recipeTitle}/>
             <p>Type: {recipeProp.recipeInfo.category}</p>
