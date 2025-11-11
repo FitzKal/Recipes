f288a39de9925c9da16d93ed82f1054a23ee3c64
Vesa Bence Janos <vesabence@mailbox.unideb.hu>
2025-11-11 14:35:33 +0100

    Implemented getById method

--- frontend/src/service/RecipeService.ts
+++ frontend/src/service/RecipeService.ts
@@ -12,3 +12,19 @@ export const getAllRecipes = async (accessToken:string)=>{
         throw new Error(message || "Something went wrong");
     }
 }
+
+// ------------- GetById -------------
+
+export const getRecipeById = async (accessToken:string, recipeId: number | undefined) =>{
+    const res = await fetch(`/api/recipes/${recipeId}`,{
+        headers: {Authorization : `Bearer ${accessToken}`},
+    });
+    if (res.ok){
+        const response = await res.json();
+        console.log(response);
+        return response;
+    }else {
+        const message = await res.text();
+        throw new Error(message || "Request could not be completed");
+    }
+}
