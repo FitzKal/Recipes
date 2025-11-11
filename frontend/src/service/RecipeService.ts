7686baa8c7c6265c5bfc8501dd4c8895dfe92ba7
Vesa Bence Janos <vesabence@mailbox.unideb.hu>
2025-11-11 14:42:08 +0100

    Implemented Post recipe method

--- frontend/src/service/RecipeService.ts
+++ frontend/src/service/RecipeService.ts
@@ -1,4 +1,6 @@
 // ------------- GetAll -------------
+import type {Recipe} from "../Types/Recipe.ts";
+
 export const getAllRecipes = async (accessToken:string)=>{
     const res = await fetch("/api/recipes",{
         headers:{Authorization:`Bearer ${accessToken}`}
@@ -18,6 +20,27 @@ export const getAllRecipes = async (accessToken:string)=>{
 export const getRecipeById = async (accessToken:string, recipeId: number | undefined) =>{
     const res = await fetch(`/api/recipes/${recipeId}`,{
         headers: {Authorization : `Bearer ${accessToken}`},
+    });
+    if (res.ok){
+        const response = await res.json();
+        console.log(response);
+        return response;
+    }else {
+        const message = await res.text();
+        throw new Error(message || "Request could not be completed");
+    }
+}
+
+// ------------- Post -------------
+
+export const postRecipe = async (accessToken:string, recipe:Recipe) =>{
+    const res = await fetch("/api/recipes",{
+        method : 'POST',
+        headers: {
+            Authorization : `Bearer ${accessToken}`,
+            "Content-Type" : "application/json"
+        },
+        body:JSON.stringify(recipe)
     });
     if (res.ok){
         const response = await res.json();
